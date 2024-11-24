387653c9fe6311690c8b1d06c164e1ac70819d13
Erik Lantto <lantto@gmail.com>
2024-11-24 21:35:16 +0100

    Make it so that it doesn't display two of the same

--- script.js
+++ script.js
@@ -4,8 +4,29 @@ const digitMessages = {
     0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: []
 };
 
+// Keep track of which messages are currently displayed
+const usedMessageIndices = new Set();
+
 function highlightNumber(text, number) {
     return `<div class="number">${number}</div>${text}`;
+}
+
+function getUnusedMessage(messages, digit) {
+    // If we have no messages, return null
+    if (messages.length === 0) return null;
+    
+    // Try to find a message that isn't currently being used
+    for (let i = messages.length - 1; i >= 0; i--) {
+        if (!usedMessageIndices.has(`${digit}-${i}`)) {
+            usedMessageIndices.add(`${digit}-${i}`);
+            return { message: messages[i], index: i };
+        }
+    }
+    
+    // If all messages are used, clear the used set and use the latest message
+    usedMessageIndices.clear();
+    usedMessageIndices.add(`${digit}-${messages.length - 1}`);
+    return { message: messages[messages.length - 1], index: messages.length - 1 };
 }
 
 function updateClockDisplay() {
@@ -15,15 +36,19 @@ function updateClockDisplay() {
     
     const slots = ['hours1', 'hours2', 'minutes1', 'minutes2', 'seconds1', 'seconds2'];
     
+    // Clear the used messages set at the start of each update
+    usedMessageIndices.clear();
+    
     digits.forEach((digit, index) => {
         const messages = digitMessages[parseInt(digit)];
         const element = document.getElementById(slots[index]);
         
-        if (messages.length > 0) {
-            const latestMessage = messages[messages.length - 1];
+        const messageData = getUnusedMessage(messages, digit);
+        
+        if (messageData) {
             element.innerHTML = `
                 <div class="hologram">
-                    ${latestMessage}
+                    ${messageData.message}
                     <div class="lines"></div>
                 </div>
             `;
